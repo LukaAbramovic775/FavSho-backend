b0cc1471d8b4a5514143707c5c4f19e2d1e019a6
Team-4-prog <60031881+LukaAbramovic775@users.noreply.github.com>
2023-08-26 14:18:07 +0200

    corrected fix connection

--- src/db.js
+++ src/db.js
@@ -1,24 +1,30 @@
-const mongo = require('mongodb');
-
+const mongodb = require("mongodb");
+ 
 let connection_string = "mongodb+srv://admin:admin@cluster0.z0t9efu.mongodb.net/?retryWrites=true&w=majority";
-let client = new mongo.MongoClient(connection_string, {
+ 
+let client = new mongodb.MongoClient(connection_string, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
-
-let db = null;
-
-export default async () => {
-    if (db && client.topology.isConnected()) {
-        return db;
-    } else {
-        try {
-            await client.connect();
-            console.log('Database connected successfully!');
-            db = client.db("FavSho");
-            return db;
-        } catch (err) {
-            throw new Error('Spajanje na bazu nije uspjelo: ' + err);
+}); 
+ 
+let db = null
+// eksportamo Promise koji resolva na konekciju
+export default () => {
+    return new Promise((resolve, reject) => {
+        // ako smo inicijalizirali bazu i klijent je još uvijek spojen
+        if (db) {
+            console.log("Database already connected!")
+            resolve(db);
+        } else {
+            try{
+                client.connect();
+                console.log('Database connected successfully!');
+                db = client.db("FavSho");
+                resolve(db);
+            } catch(e) {
+                console.log(e)
+                reject('Database not connected! ' + e);
+            }
         }
-    }
-};
+    });
+}
