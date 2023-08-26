3445410ad78f220bc2d572d37e62d3be8560ea37
Team-4-prog <60031881+LukaAbramovic775@users.noreply.github.com>
2023-08-26 13:06:15 +0200

    database client connetion fixed

--- src/auth.js
+++ src/auth.js
@@ -8,7 +8,7 @@ import jwt from "jsonwebtoken";
 // Kreiranje indeksa pri pokretanju aplikacije (ukoliko već ne postoji)
 (async () => {
   let db = await connect();
-  db.collection("users").createIndex({ username: 1 }, { unique: true });
+  db.collection("users").createIndex({ email: 1 }, { unique: true });
 })();
 export default {
   async registerUser(userData) {
@@ -16,16 +16,15 @@ export default {
     let result;
     try {
       let doc = {
-        username: userData.username,
+        email: userData.email,
         // lozinku ćemo hashirati pomoću bcrypta
         password: await bcrypt.hash(userData.password, 4),
-        name: userData.name,
       };
           result = await db.collection("users").insertOne(doc);
     } catch (e) {
-      if (e.name == "MongoError") {
+      if (e.email == "MongoError") {
         if (e.code == 11000) {
-          throw new Error("Username already exists");
+          throw new Error("email already exists");
         }
       }
     }
@@ -35,9 +34,9 @@ export default {
       throw new Error("Cannot register user");
     }
   },
-  async authenticateUser(username, password) {
+  async authenticateUser(email, password) {
     let db = await connect();
-    let user = await db.collection("users").findOne({ username: username });
+    let user = await db.collection("users").findOne({ email: email });
     if (user && user.password && (await bcrypt.compare(password, user.password))) {
       delete user.password; // ne želimo u tokenu, token se sprema na klijentu
       let token = jwt.sign(user, process.env.JWT_SECRET, {
@@ -47,7 +46,7 @@ export default {
 
     return {
       token,
-      username: user.username,
+      email: user.email,
     };
     } else {
       throw new Error("Cannot authenticate");
--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
 import express from "express";
-
 import mongo from "mongodb";
 import connect from "./db.js";
 import auth from "./auth";
+import cors from "cors";
 
 const app = express(); // instanciranje aplikacije
 const port = 3000; // port na kojem će web server slušati
 
-const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 // ne možeš pristupiti tajni ako ne prođeš `auth.verify`
-// app.get("/tajna", [auth.verify], async (req, res) => {
-//   // nakon što se izvrši auth.verify middleware, imamo dostupan req.jwt objekt
-//   res.status(200).send("tajna korisnika " + req.jwt.username);
-// });
+app.get("/tajna", [auth.verify], async (req, res) => {
+// nakon što se izvrši auth.verify middleware, imamo dostupan req.jwt objekt
+   res.status(200).send("tajna korisnika " + req.jwt.email);
+});
 
 //Registracija
-app.post("/korisnik", async (req, res) => {
-  let user = req.body;
-
-  try {
-    let result = await auth.registerUser(user);
-    res.status(201).send();
-  } catch (e) {
-    res.status(500).json({
-      error: e.message,
-    });
+app.post('/user', async (req , res) =>{
+  let userData = req.body;
+  let id;
+  try{
+      id = await auth.registerUser(userData);
   }
+  catch(e){
+      res.status(500).json({ error: e.message });
+  }
+
+  res.json({ id:id })
+
 });
 
 //Prijava
 app.post("/auth", async (req, res) => {
   let user = req.body;
-  let username = user.username;
-  let password = user.password;
+  let userEmail = user.email 
+  let userPassword = user.password 
   try {
-    let result = await auth.authenticateUser(username, password);
+    let result = await auth.authenticateUser(userEmail, userPassword);
     res.status(201).json(result);
   } catch (e) {
     res.status(500).json({
@@ -45,11 +45,44 @@ app.post("/auth", async (req, res) => {
   }
 });
 
-// popis serija
-app.get('/show', async (req , res) =>{
+//slanje serija
+app.post('/series', async (req , res) =>{
   let db = await connect();
 
-  let cursor = await db.collection('series').find({});
+  let showData = req.body;
+
+  let result = await db.collection('series').insertOne(showData);
+  if (result.insertedCount == 1) {
+      res.send({
+          status: 'success',
+          id: result.insertedId,
+      });
+  } 
+  else {
+      res.send({
+          status: 'fail',
+      });
+  }
+
+  console.log(result);
+
+});
+
+// popis serija
+app.get('/series', async (req , res) =>{
+  let db = await connect();
+
+  let cursor = await db.collection('series').find();
+  let series = await cursor.toArray();
+  
+  res.json(series);
+});
+
+app.get('/', async (req , res) =>{
+  console.log("cekam konekciju");
+  let db = await connect();
+  console.log("cekam konekciju");
+  let cursor = await db.collection('series').find();
   let series = await cursor.toArray();
   
   res.json(series);
